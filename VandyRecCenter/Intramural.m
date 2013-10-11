cb177edbbb8556fef1c560b70cbc4ab74b713ac3
Brendan McNamara <brendan@streampiece.com>
2013-10-11 14:11:31 -0500

    Adds intramural model

---
+++ VandyRecCenter/Intramural.m
@@ -0,0 +1,13 @@
+//
+//  Intramural.m
+//  VandyRecCenter
+//
+//  Created by Brendan McNamara on 10/11/13.
+//  Copyright (c) 2013 Brendan McNamara. All rights reserved.
+//
+
+#import "Intramural.h"
+
+@implementation Intramural
+
+@end
