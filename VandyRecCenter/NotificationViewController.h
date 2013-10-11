24e5f08afe8acdaeaa4dbee7d3ec25b97bf08a54
Brendan McNamara <brendan@streampiece.com>
2013-10-10 20:11:39 -0500

    Createscontrol cell for Notification table view

--- VandyRecCenter/NotificationViewController.h
+++ VandyRecCenter/NotificationViewController.h
@@ -17,6 +17,12 @@
 #define CELL_ICON_TAG 3
 #define CELL_MESSAGE_TAG 4
 
+#define CONTROL_CELL_CONTENTVIEW 1
+#define CONTROL_CELL_NEWS_BUTTON 2
+#define CONTROL_CELL_HOURS_BUTTON 3
+#define CONTROL_CELL_GROUPFITNESS_BUTTON 5
+#define CONTROL_CELL_INTRAMURALS_BUTTON 4
+
 @interface NotificationViewController : UIViewController <UITableViewDataSource, UITableViewDelegate, NoticiationDelegate>
 
 //color mapping that corresponds a color to a notification type
@@ -27,5 +33,11 @@
 @property (nonatomic, strong, readonly) NSArray* colorMap;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (nonatomic, strong) MBProgressHUD* HUD;
+
+
+- (IBAction) toggleNews:(id)sender;
+- (IBAction) toggleHours:(id)sender;
+- (IBAction) toggleGroupFitness:(id)sender;
+- (IBAction) toggleIntramurals:(id)sender;
 
 @end
