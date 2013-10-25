0f21fe0427ecbd8f562a129ff82fc834efc65eea
Aaron Smith <asmith1810@gmail.com>
2013-10-25 17:56:10 -0500

    Wrote reduce, forEach, and filter blocks

--- NSArray+MyArrayClass.m
+++ NSArray+MyArrayClass.m
@@ -10,5 +10,43 @@
 
 @implementation NSArray(MyArrayClass)
 
+// Pass each element of the array into a callback function.
+// If the callback function returns true, place the element in
+// the resulting array.
+- (NSArray*) filter: (BOOL (^)(id element, NSUInteger index)) block {
+    NSMutableArray* filteredArray = [[NSMutableArray alloc] init];
+    
+    for (id element in self) { // Is self the array?
+        NSUInteger elementIndex = [self indexOfObject:element];
+        if (block(element, elementIndex)) [filteredArray addObject:element];
+    }
+    
+    return filteredArray;
+};
+
+- (id) reduce: (id (^)(id element, NSUInteger index)) block {
+    id element;
+    
+    if (!element || ![element count]) { // check that there are methods in array?
+        [NSException raise:@"Invalid Array" format:@"Array is empty or null"];
+    }
+    
+    id reducedValue;
+    
+    for (element in self) {
+        NSUInteger elementIndex = [self indexOfObject:element];
+        reducedValue = block(element, elementIndex);
+    }
+    
+    return reducedValue;
+    
+};
+
+- (void) forEach: (BOOL (^)(id element, NSUInteger index)) block {
+    for (id element in self) {
+        NSUInteger elementIndex = [self indexOfObject:element];
+        block(element, elementIndex);
+    }
+};
 
 @end
