e6b48444ca0bdad974f70b76627a0a3804de058e
Brendan McNamara <bahai9@live.com>
2013-12-12 19:49:24 -0600

    Adds GFFavorites view and view controller

---
+++ VandyRecCenter/GFFavoritesViewController.h
@@ -0,0 +1,16 @@
+//
+//  GFFavoritesViewController.h
+//  VandyRecCenter
+//
+//  Created by Brendan McNamra on 12/12/13.
+//  Copyright (c) 2013 Brendan McNamara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GFFavoritesViewController : UIViewController
+
+
+@property (nonatomic, weak) IBOutlet UIView* headerView;
+@property (nonatomic, weak) IBOutlet UITableView* tableView;
+@end
---
+++ VandyRecCenter/GFFavoritesViewController.m
@@ -0,0 +1,35 @@
+//
+//  GFFavoritesViewController.m
+//  VandyRecCenter
+//
+//  Created by Brendan McNamra on 12/12/13.
+//  Copyright (c) 2013 Brendan McNamara. All rights reserved.
+//
+
+#import "GFFavoritesViewController.h"
+
+@interface GFFavoritesViewController ()
+
+@end
+
+@implementation GFFavoritesViewController
+
+
+@synthesize headerView = _headerView;
+@synthesize tableView = _tableView;
+
+#pragma mark - Lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
