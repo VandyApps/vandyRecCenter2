fff8d0011df43b0da74d8e3f1c690609433daadb
Brendan McNamara <bahai9@live.com>
2013-12-14 22:04:09 -0600

    Adds error handling class

---
+++ VandyRecCenter/ErrorHandler.h
@@ -0,0 +1,18 @@
+//
+//  ErrorHandler.h
+//  VandyRecCenter
+//
+//  Created by Brendan McNamra on 12/14/13.
+//  Copyright (c) 2013 Brendan McNamara. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString* NetworkErrorConnection;
+extern NSString* NetworkErrorUnknown;
+
+@interface ErrorHandler : NSObject
+
++ (void) handleError: (NSError*) error withResponse: (NSHTTPURLResponse*) response type: (NSString*) errorType;
+
+@end
---
+++ VandyRecCenter/ErrorHandler.m
@@ -0,0 +1,21 @@
+//
+//  ErrorHandler.m
+//  VandyRecCenter
+//
+//  Created by Brendan McNamra on 12/14/13.
+//  Copyright (c) 2013 Brendan McNamara. All rights reserved.
+//
+
+#import "ErrorHandler.h"
+
+NSString* NetworkErrorConnection = @"NetworkErrorConnection";
+NSString* NetworkErrorUnknown = @"NetworkErrorUnknown";
+
+@implementation ErrorHandler
+
+
++ (void) handleError:(NSError *)error withResponse:(NSHTTPURLResponse *)response type:(NSString *)errorType
+{
+    
+}
+@end
