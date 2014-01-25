3b89988d3e525eb6b303c2b8e5493513e69206ef
Brendan McNamara <bahai9@live.com>
2014-01-25 13:29:35 -0600

    Adds in files for intramurals

---
+++ VandyRecCenter/LeagueViewController.h
@@ -0,0 +1,15 @@
+//
+//  LeagueViewController.h
+//  VandyRecCenter
+//
+//  Created by Brendan McNamra on 1/23/14.
+//  Copyright (c) 2014 Brendan McNamara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LeagueViewController : UIViewController
+
+@property (nonatomic, weak) IBOutlet UISegmentedControl* segmentedControl;
+
+@end
---
+++ VandyRecCenter/PlayoffsViewController.m
@@ -0,0 +1,38 @@
+//
+//  IntramuralPlayoffsViewController.m
+//  VandyRecCenter
+//
+//  Created by Brendan McNamra on 1/24/14.
+//  Copyright (c) 2014 Brendan McNamara. All rights reserved.
+//
+
+#import "PlayoffsViewController.h"
+
+@interface PlayoffsViewController ()
+
+@end
+
+@implementation PlayoffsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	self.view.backgroundColor = [UIColor redColor];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
